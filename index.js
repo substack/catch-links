e17f6abd8b64e9224eff52356c47e82f0186b502
James Halliday <mail@substack.net>
2015-07-01 12:30:46 -0700

    relative-only

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var url = require('url');
 
-module.exports = function (root, cb, rel) {
+module.exports = function (root, cb) {
     root.addEventListener('click', function (ev) {
         if (ev.altKey || ev.ctrlKey || ev.metaKey || ev.shiftKey || ev.defaultPrevented) {
             return true;
         }
-
+        
         var anchor = null;
         for (var n = ev.target; n.parentNode; n = n.parentNode) {
             if (n.nodeName === 'A') {
@@ -14,19 +14,17 @@ module.exports = function (root, cb, rel) {
             }
         }
         if (!anchor) return true;
-
+        
         var href = anchor.getAttribute('href');
         var u = url.parse(anchor.getAttribute('href'));
-
+        
         if (u.host && u.host !== location.host) return true;
-
+        
         ev.preventDefault();
-
-        var link = rel === true
-            ? u.path + (u.hash || '')
-            : url.resolve(location.href, href);
-
-        cb(link);
+        
+        var base = location.protocol + '//' + location.host;
+        
+        cb(url.resolve(location.pathname, u.path) + (u.hash || ''));
         return false;
     });
 };
